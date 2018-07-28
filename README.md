21923eaea7b089d4e234e51b20a5c2e7787f5526
ZeroingIn <zeroing0in@gmail.com>
2018-07-28 17:41:58 +0800

    First Blood

---
+++ README.md
@@ -0,0 +1,27 @@
+#### 开机自动解锁
+
+此模块会在开机时自动输入手机和SIM卡的PIN。
+
+#### 注意事项
+
+* 通过修改config文件自定义PIN的功能尚未完成，请自行clone或者fork修改代码重新打包安装模块。
+* 请使用最新的Magisk来安装此模块。
+* 最近更新:
+Magisk v16 Template 1600 compatabilities
+
+#### Auto Input Boot PIN
+
+This module will auto input phone/SIM PIN on phone startup.
+
+#### NOTICE
+
+* The function of customizing the PIN by modifying the config file has not been completed. Please re-package the installation module by clone or fork modify the code.
+* You should use latest Magisk Manager to install this module. If you meet any problem under installation from Magisk Manager, please try to install it from recovery.
+* Recent fixes:
+Magisk v16 Template 1600 compatabilities
+
+#### Credit & Support
+
+* Copyright (C) 2018 simonsmh <zeroing0in@gmail.com>
+* Any issue or pull request is welcomed.
+* Star this module at [GitHub](https://github.com/ZeroingIn/AutoInputBootPin).
---
+++ common/service.sh
@@ -0,0 +1,61 @@
+#!/system/bin/sh
+MODDIR=${0%/*}
+#encryption detection
+# [ $(getprop ro.crypto.state) = "encrypted" ] || echo "unencrypted"
+#boot detection
+until [ $(getprop init.svc.bootanim) = "stopped" ]
+do
+sleep 2
+done
+
+# 7 -->  "KEYCODE_0" 48
+# 16 -->  "KEYCODE_9" 57
+# 66 -->  "KEYCODE_ENTER"
+
+#convert char to keyCode
+function convert()
+{
+    #A-Za-z0-9
+    char=$1
+    charASCII=$( printf "%d" "'${char}" )
+    keyCode=$((10#${charASCII}-41))
+    echo $keyCode
+    return $keyCode
+}
+
+#source /sdcard/auto-input-boot-pin/config
+#input sim 1 pin
+# for i in `seq 1 ${#PIN1}`
+# do
+# c=${PIN1:$i-1:1}
+# echo $c
+# code=$(convert $c)
+# input keyevent $code
+# done
+sleep 2
+input text 123456
+input keyevent 66
+sleep 2
+
+#input sim 2 pin
+# for i in `seq 1 ${#PIN2}`
+# do
+# c=${PIN2:$i-1:1}
+# echo $c
+# code=$(convert $c)
+# input keyevent $code
+# done
+input text 567890
+input keyevent 66
+sleep 2
+
+#input phone pin
+# for i in `seq 1 ${#PIN3}`
+# do
+# c=${PIN3:$i-1:1}
+# echo $c
+# code=$(convert $c)
+# input keyevent $code
+# done
+input text 9999
+input keyevent 66
